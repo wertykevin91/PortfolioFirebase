f563985532df276f757ad12a14c704e09d73e996
Kevin Wong <wertykevin91@gmail.com>
2018-03-25 02:43:17 +0800

    Added base css, js and html

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -5,43 +5,34 @@ const functions = require('firebase-functions');
 
 const router = express.Router();
 
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
+// const firebaseApp = firebase.initializeApp(
+//     functions.config().firebase
+// );
 
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
-function checkDb(){
-    const ref = db.collection('values');
-    var docRef = ref.doc('donkey');
-    var setDonkey = docRef.set({
-        FullName: 'Donkey',
-        ContactNo: '+15157083249',
-        State: 'Texas',
-        Country: 'United States',
-        Affiliation: 'Dragon'
-    });
-}
-checkDb();
+// function checkDb(){
+//     const ref = db.collection('values');
+//     var docRef = ref.doc('donkey');
+//     var setDonkey = docRef.set({
+//         FullName: 'Donkey',
+//         ContactNo: '+15157083249',
+//         State: 'Texas',
+//         Country: 'United States',
+//         Affiliation: 'Dragon'
+//     });
+// }
+// //checkDb();
 
-function getValues(){
-    const ref = db.collection('values');
-    return ref.get().then(snapshot=> snapshot.docs);
-}
+// function getValues(){
+//     const ref = db.collection('values');
+//     return ref.get().then(snapshot=> snapshot.docs);
+// }
 
 /* GET home page. */
-router.get('/', (req, res) => {
-    getValues().then(values => {
-        var vals = [];
-        values.forEach(doc=>{
-            vals.push(doc.data());
-        });
-        res.render('index/index', { title: 'Kevin Portfolio', values: vals });
-        return;
-    }).catch(ex=>{
-        console.log(ex);
-    });
-    
+router.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-max-age=1800');
+    response.render('index/index', { title: 'Kevin Wong'});
 });
 
 module.exports = router;
